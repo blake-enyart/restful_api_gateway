1f5295e5f8aebfba0c084775e8393da05ec1b8c3
Blake Enyart <blake.enyart@nutrien.com>
2020-12-03 21:12:13 -0700

    Refactored and successfully configured code for API deployments

---
+++ api/main.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI
+from v1.routers import router
+from mangum import Mangum
+
+app = FastAPI(
+    title="Cryptocurrency API",
+    description="API to track current prices and trading signals",
+    openapi_prefix="/prod",  # name of your stage
+)
+app.include_router(router, prefix="/v1")
+
+
+@app.get("/")
+def read_root():
+    return {"Hello Medium Reader": "from FastAPI & API Gateway"}
+
+
+# to make it work with Amazon Lambda, we create a handler object
+handler = Mangum(app=app)
--- dbg_pds_data_pipeline_practice/dbg_pds_data_pipeline_practice_stack.py
+++ dbg_pds_data_pipeline_practice/dbg_pds_data_pipeline_practice_stack.py
@@ -5,39 +5,54 @@ from aws_cdk import (
     aws_sns_subscriptions as subs,
     aws_s3 as s3,
     aws_lambda as _lambda,
+    aws_lambda_event_sources as _lambda_event,
+    aws_ec2 as ec2,
     core,
 )
 
-from aws_solutions_constructs import aws_s3_lambda as s3_lambda
+# from aws_solutions_constructs import (
+#     aws_s3_lambda as s3_lambda,
+#     aws_lambda_sqs_lambda as lsl,
+# )
 
 
 class DbgPdsDataPipelinePracticeStack(core.Stack):
     def __init__(self, scope: core.Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        s3_bucket = s3.Bucket.from_bucket_arn(
-            self, "TestBucket", bucket_arn="arn:aws:s3:::deutsche-boerse-eurex-pds"
-        )
+        vpc = ec2.Vpc(self, "MyVpc")
 
-        s3_bucket_test = s3.Bucket(self, "TestBucketTwo")
+        vpc.select_subnets(subnet_type=ec2.SubnetType.PUBLIC)
 
-        s3_lambda.S3ToLambda(
-            self,
-            "S3ToLambdaTest",
-            existing_bucket_obj=s3_bucket_test,
-            lambda_function_props=_lambda.FunctionProps(
-                code=_lambda.Code.asset("lambda"),
-                handler="test.handler",
-                runtime=_lambda.Runtime.PYTHON_3_8,
-            ),
-        )
-
-        # queue = sqs.Queue(
+        # s3_bucket = s3.Bucket(
         #     self,
-        #     "DbgPdsDataPipelinePracticeQueue",
-        #     visibility_timeout=core.Duration.seconds(300),
+        #     "TestBucketTwo",
+        #     bucket_name="blake-enyart-test-data-pipeline",
         # )
 
-        # topic = sns.Topic(self, "DbgPdsDataPipelinePracticeTopic")
+        # s3_fn = s3_lambda.S3ToLambda(
+        #     self,
+        #     "S3ToLambdaTest",
+        #     existing_bucket_obj=s3_bucket,
+        #     lambda_function_props=_lambda.FunctionProps(
+        #         code=_lambda.Code.asset("lambda"),
+        #         handler="test.handler",
+        #         runtime=_lambda.Runtime.PYTHON_3_8,
+        #     ),
+        #     s3_event_source_props=_lambda_event.S3EventSourceProps(
+        #         events=[s3.EventType.OBJECT_CREATED]
+        #     )
+        # )
 
-        # topic.add_subscription(subs.SqsSubscription(queue))
+        # lsl.LambdaToSqsToLambda(self,
+        #     "LambdaToSQSToLambda",
+        #     existing_producer_lambda_obj=s3_fn.lambda_function,
+        #     consumer_lambda_function_props=_lambda.FunctionProps(
+        #         code=_lambda.Code.asset("lambda"),
+        #         handler="consumer.handler",
+        #         runtime=_lambda.Runtime.PYTHON_3_8,
+        #     )
+        # )
+
+
+# aws s3 sync s3://deutsche-boerse-eurex-pds/2017-05-27/ s3://blake-enyart-test-data-pipeline/
